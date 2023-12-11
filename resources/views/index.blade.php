c7198a4ffab1b743be6afea71c83fa7de3c72a9f
Doginnn <diogenesemmanuel@gmail.com>
2023-12-11 18:18:56 -0300

    [UPD] Criando cards para cadastro e edição de clientes na tela inicial.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,9 +13,27 @@
     <div class="row">
         <div class="col-md-6 col-lg-3">
             <div class="widget-small primary coloured-icon">
-                <a href="{{ route('clients.index') }}"><i class="icon fa fa-users fa-3x"></i></a>
+                <a href="{{ route('clients.create') }}"><i class="icon fa fa-user-plus fa-3x"></i></a>
                 <div class="info">
-                    <h4>Clientes Cadastrados</h4>
+                    <h4><b>Novo Cliente</b></h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+            <div class="widget-small danger coloured-icon">
+                <a href="{{ route('clients.index') }}"><i class="icon bi bi-person-fill-gear fa-3x"></i></a>
+                <div class="info">
+                    <h4><b>Editar Clientes</b></h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+            <div class="widget-small info coloured-icon">
+                <i class="icon fa fa-users fa-3x"></i>
+                <div class="info">
+                    <h4><b>Clientes Cadastrados</b></h4>
                     <p><b>{{ DB::table('clients')->count() }}</b></p>
                 </div>
             </div>
